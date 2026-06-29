853dbd89aab9987d27e26ffd992e86d9ad84fb50
MuchenXu-8as <15002761720@163.com>
2026-06-29 21:44:33 +0800

    Add persistent storage support for Render disk

--- server/db.js
+++ server/db.js
@@ -2,15 +2,14 @@ const path = require('path');
 const fs = require('fs');
 const Database = require('better-sqlite3');
 
-
-const DATA_DIR = path.join(__dirname, '..', 'data');
+const DATA_DIR = process.env.RENDER_DISK_PATH
+  ? path.join(process.env.RENDER_DISK_PATH, 'data')
+  : path.join(__dirname, '..', 'data');
 if (!fs.existsSync(DATA_DIR)) fs.mkdirSync(DATA_DIR, { recursive: true });
-
 
 const dbPath = path.join(DATA_DIR, 'nordic-lamp.db');
 const db = new Database(dbPath);
 db.pragma('journal_mode = WAL');
 db.pragma('foreign_keys = ON');
-
 
 module.exports = db;
